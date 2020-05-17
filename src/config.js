1f177b78b877e2f6848c3ae345f9e83de36b77ac
Theint Htet Htet Aung <theinthtethtetaung99@gmail.com>
2020-05-17 21:55:01 +0630

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "thhabucket",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://rwx10dwfm8.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_2XbcaxOhv",
+    APP_CLIENT_ID: "hs8vlqbcitss2mk2v7v2cceca",
+    IDENTITY_POOL_ID: "us-east-1:fd38654b-30ec-48b4-af5f-e91edfab5cae",
+  },
+};
