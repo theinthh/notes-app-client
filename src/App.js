080e9e60519a40b5034e44468204e3da4cb09810
Theint Htet Htet Aung <theinthtethtetaung99@gmail.com>
2020-05-25 21:55:35 +0630

    Updating the lander

--- src/App.js
+++ src/App.js
@@ -17,27 +17,27 @@ function App() {
   useEffect(() => {
     //only re-run our function (edit)
     //if the passed in array of variables have changed
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-    loadFacebookSDK();
+   loadFacebookSDK();
     onLoad();
   }, []);
 
+  function (d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+          return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      })(document, "script", "facebook-jssdk");
+ 
   function loadFacebookSDK() {
     window.fbAsyncInit = function () {
       window.FB.init({
         appId: config.social.FB,
         cookie: true,
-        status: true,
         xfbml: true,
         version: "v7.0",
       });
